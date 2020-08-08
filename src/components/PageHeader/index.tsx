a784d785b27be87051a45d098dd03c2ff7961fe1
Lucas Siqueira Fernandes <lucassiqueirafernandes@MacBook-Air-de-Lucas.local>
2020-08-08 17:10:04 -0300

    Setting final layout

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -9,6 +9,7 @@ import './styles.css';
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -26,6 +27,8 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
                 <strong>
                     { props.title }
                 </strong>
+
+                { props.description && <p>{ props.description }</p> }
             </div>
 
             { props.children }
